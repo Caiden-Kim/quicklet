c5092a5edf09237733852c12e209341a57594606
Caiden Kim <kimc1145@oshkosh.k12.wi.us>
2025-11-20 00:10:10 -0600

    e

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,15 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import SetsPage from "@/pages/SetsPage.vue";
-import FlashcardsPage from "@/pages/FlashcardsPage.vue";
-import QuizPage from "@/pages/QuizPage.vue";
-
-const routes = [
-  { path: "/", component: SetsPage },
-  { path: "/flashcards/:id", component: FlashcardsPage, props: true },
-  { path: "/quiz/:id", component: QuizPage, props: true },
-];
+import SetsPage from "@/views/SetsPage.vue";
+import FlashcardsPage from "@/views/FlashcardsPage.vue";
+import QuizPage from "@/views/QuizPage.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+  history: createWebHashHistory(), // HASH MODE
+  routes: [
+    { path: "/", component: SetsPage },
+    { path: "/flashcards/:id", component: FlashcardsPage, props: true },
+    { path: "/quiz/:id", component: QuizPage, props: true },
+  ],
 });
 
 export default router;
